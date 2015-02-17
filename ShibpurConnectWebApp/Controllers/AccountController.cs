ffce5a01d757e869a5498a8c50ed7a1cd6c9e556
Pritam Karmakar <pritam83@gmail.com>
2015-02-16 23:59:24 -0800

    Removed bin/obj

--- ShibpurConnectWebApp/Controllers/AccountController.cs
+++ ShibpurConnectWebApp/Controllers/AccountController.cs
@@ -405,7 +405,7 @@ namespace ShibpurConnectWebApp.Controllers
             switch (result)
             {
                 case SignInStatus.Success:
-                    return RedirectToLocal(returnUrl);
+                    return RedirectToAction("Index", "Feed");
                 case SignInStatus.LockedOut:
                     return View("Lockout");
                 case SignInStatus.RequiresVerification:
