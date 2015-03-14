2feeefe44569475b6472931865695db0f91b0ddc
Pritam Karmakar <pritam83@gmail.com>
2015-03-14 02:49:08 -0700

    Added user profile page. have to work on the css

---
+++ ShibpurConnectWebApp/Models/WebAPI/Answer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.Serialization;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
+
+namespace ShibpurConnectWebApp.Models.WebAPI
+{
+    public class Answer
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        [JsonProperty(PropertyName = "answerId")]
+        public string AnswerId { get; set; }
+
+        [DataMember]
+        public string UserId { get; set; }
+
+        [DataMember]
+        public string QuestionId { get; set; }
+
+        [DataMember]
+        public string AnswerText { get; set; }
+
+        [DataMember]
+        public bool MarkedAsAnswer { get; set; }
+
+        [DataMember]
+        public int UpVoteCount { get; set; }
+
+        [DataMember]
+        public int DownVoteCount { get; set; }
+
+        [DataMember]
+        public DateTime PostedOnUtc { get; set; }
+    }
+}
---
+++ ShibpurConnectWebApp/Models/WebAPI/UserProfile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ShibpurConnectWebApp.Models.WebAPI
+{
+    /// <summary>
+    /// Use this model for populating the user profile page with educational history and employment history
+    /// </summary>
+    public class UserProfile
+    {
+        public CustomUserInfo UserInfo { get; set; }
+        public List<EducationalHistories> EducationalHistories { get; set; }
+        public List<EmploymentHistories> EmploymentHistories { get; set; } 
+    }
+}
