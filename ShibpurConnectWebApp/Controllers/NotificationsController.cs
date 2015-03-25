27d04b4fa59e76cf751e39db431d21cae970d851
Pritam Karmakar <pritam83@gmail.com>
2015-03-25 09:32:13 -0700

    Now we have a notification page to view all notifications

---
+++ ShibpurConnectWebApp/Controllers/NotificationsController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShibpurConnectWebApp.Controllers
+{
+    public class NotificationsController : Controller
+    {
+        // GET: Notifications
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Notifications/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Notifications/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Notifications/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Notifications/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Notifications/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Notifications/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Notifications/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
