613c48a1fe52d8ec7c23d207d0c47763db504af5
Pritam Karmakar <pritam83@gmail.com>
2015-03-28 17:27:49 -0700

    changed Account/Finduser api call to synchronous, as it was not working in Firefox

--- ShibpurConnectWebApp/Controllers/AccountController.cs
+++ ShibpurConnectWebApp/Controllers/AccountController.cs
@@ -546,7 +546,7 @@ namespace ShibpurConnectWebApp.Controllers
         public ActionResult LogOff()
         {
             AuthenticationManager.SignOut();
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login", "Account");
         }
 
         //
