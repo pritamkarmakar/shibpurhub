7cda4c283145a1b651627d94c3d21a48492af2ed
Pritam Karmakar <pritam83@gmail.com>
2015-07-03 03:12:51 -0700

    Fixing last build break, adding missing class

---
+++ ShibpurConnectWebApp/Controllers/SlugToIdAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Controllers;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ShibpurConnectWebApp.Controllers
+{
+    /// <summary>
+    /// Filter to retrieve the question id using the seo friendly url (slug)
+    /// </summary>
+    public class SlugToIdAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            var slug = filterContext.RouteData.Values["id"] as string;
+            if (slug != null)
+            {
+                // retrieve the questionid from database
+                Helper.Helper helper = new Helper.Helper();
+                var questionId = helper.GetQuestionIdFromSlug(slug);
+                if (questionId != null)
+                    filterContext.ActionParameters["id"] = questionId;
+            }
+            base.OnActionExecuting(filterContext);
+        }
+
+        
+    }
+}
