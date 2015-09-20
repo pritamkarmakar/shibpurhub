e6af49fcc8a7b68aca86953fa01a4c868451fa4a
Pritam Karmakar <pritam83@gmail.com>
2015-09-20 05:33:29 -0700

    Mobile friendly views. Started with feed page.

---
+++ ShibpurConnectWebApp/Controllers/BlogsController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShibpurConnectWebApp.Controllers
+{
+    public class BlogsController : Controller
+    {
+        // GET: Blogs
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Blogs/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Blogs/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Blogs/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Blogs/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Blogs/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Blogs/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Blogs/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ ShibpurConnectWebApp/Routes/SubdomainRoute.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace ShibpurConnectWebApp.Routes
+{
+    class SubdomainRoute : RouteBase
+    {
+        public override RouteData GetRouteData(HttpContextBase httpContext)
+        {
+            var url = httpContext.Request.Headers["HOST"];
+            var index = url.IndexOf(".");
+            Trace.Write("Hey, this is nice");
+            if (index < 0)
+                return null;
+
+            var subDomain = url.Substring(0, index);
+
+            if (subDomain == "blogs")
+            {
+                var routeData = new RouteData(this, new MvcRouteHandler());
+
+                routeData.Values.Add("controller", "Blogs");
+                routeData.Values.Add("action", "Index");
+                //routeData.Values.Add("deptName", subDomain);
+
+                return routeData;
+            }
+
+            return null;
+        }
+
+        public override VirtualPathData GetVirtualPath(RequestContext requestContext, RouteValueDictionary values)
+        {
+            //Implement your formating Url formating here
+            return null;
+        }
+    }
+}
